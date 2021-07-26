03dce63b0e0160a7088ddd5fe6afd05d8d8f5d46
Niels Hoogendoorn <>
2021-07-26 15:00:05 +0200

    chore: initial files

---
+++ CodeReview/CodeReview/CodeReviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeReviewApp.swift
+//  CodeReview
+//
+//  Created by Niels Hoogendoorn on 26/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct CodeReviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
